024134297cd4a80ca00add4fef99af2e037db9cc
ingwon97 <ghkdrnjs180@naver.com>
2023-05-05 12:55:40 +0900

    Initial commit

---
+++ src/java/controller/TestController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @GetMapping("/test")
+    public String test() {
+        return "hello test";
+    }
+}
